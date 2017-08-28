5b3d6f6608f463e4aa0bebb6da102acc85458cf9
Marcin Mirecki <mmirecki@redhat.com>
2017-08-28 13:19:29 +0200

    Modify automation/check-patch to trigger build artifacts when Makefile or spec changes
    Change-Id: I2f78480733c0b965581edc79230f3504110908f1
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -6,3 +6,7 @@ pip install -U requests-mock
 
 make check
 make unittest
+
+if git diff-tree --no-commit-id --name-only -r HEAD | egrep --quiet 'vdsm.spec.in|Makefile.am' ; then
+    ./automation/build-artifacts.sh
+fi
