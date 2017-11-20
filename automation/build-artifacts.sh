1dfefe3973aa86f82391f40d5199d50abbca5c9b
Marcin Mirecki <mmirecki@redhat.com>
2017-11-20 14:32:04 +0100

    automation: install rpm if spec or make files changes
    This patch attempts to install the provider rpm's
    if the spec file or make file or automation subdirectory have changed.
    
    Change-Id: I195386edff3fea0cab25100c8ddafd6985bb0e30
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>
    Bug-Url: https://bugzilla.redhat.com/1505398

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -7,3 +7,4 @@ make rpm
 
 cp ovirt-provider-ovn-*.tar.gz exported-artifacts/
 cp ~/rpmbuild/RPMS/noarch/ovirt-provider-ovn-*.noarch.rpm exported-artifacts/
+yum -y install exported-artifacts/ovirt-provider-ovn-*.noarch.rpm
--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -7,6 +7,6 @@ pip install -U requests-mock
 make check
 make unittest
 
-if git diff-tree --no-commit-id --name-only -r HEAD | egrep --quiet 'vdsm.spec.in|Makefile.am' ; then
+if git diff-tree --no-commit-id --name-only -r HEAD | egrep --quiet 'ovirt-provider-ovn.spec.in|Makefile|automation' ; then
     ./automation/build-artifacts.sh
 fi
