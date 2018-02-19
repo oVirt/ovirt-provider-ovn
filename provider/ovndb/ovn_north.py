oVirt/ovirt-provider-ovn

commit 1d6313bde943e44fe523a5887f13cfa32ce289db

Remove redundancy
Currently the localnet network name and VLAN tag is stored not
only in the lsp, which is required by OVN, but also in the ls.
This change creates the information required for network from
the lsp and gets rid of the redundant storage in ls.

Change-Id: I93a163a5e8787650a161b6fa7b153dc2e5dd0746
Signed-off-by: Dominik Holler <dholler@redhat.com>

provider/ovndb/ovn_north.py
@@ -42,6 +42,7 @@ from ovirt_provider_config_common import ssl_cert_file
 from ovndb.db_set_command import DbSetCommand
 from ovndb.ovn_north_mappers import AddRouterInterfaceMapper
 from ovndb.ovn_north_mappers import NetworkMapper
+from ovndb.ovn_north_mappers import Network
 from ovndb.ovn_north_mappers import NetworkPort
 from ovndb.ovn_north_mappers import PortMapper
 from ovndb.ovn_north_mappers import RemoveRouterInterfaceMapper
@@ -92,9 +93,16 @@ class OvnNorth(object):
 
     @NetworkMapper.map_to_rest
     def list_networks(self):
-        return self._execute(self.idl.ls_list())
+        ls_rows = self._execute(self.idl.ls_list())
+        return [self._get_network(ls) for ls in ls_rows]
 
-    def _get_network(self, network_id):
+    def _get_network(self, ls):
+        return Network(
+            ls=ls,
+            localnet_lsp=self._get_localnet_lsp(ls)
+        )
+
+    def _get_ls(self, network_id):
         network = self._execute(self.idl.ls_get(network_id))
         if not network:
             raise ElementNotFoundError(
@@ -104,7 +112,9 @@ class OvnNorth(object):
 
     @NetworkMapper.map_to_rest
     def get_network(self, network_id):
-        return self._get_network(network_id)
+        return self._get_network(
+            self._get_ls(network_id)
+        )
 
     @NetworkMapper.validate_add
     @NetworkMapper.map_from_rest
@@ -118,16 +128,17 @@ class OvnNorth(object):
             return self._add_network(name)
 
     def _add_network(self, name):
-        return self._execute(self.idl.ls_add(switch=name, may_exist=False))
+        return self._get_network(
+            self._execute(self.idl.ls_add(switch=name, may_exist=False))
+        )
 
     def _add_localnet_network(self, name, localnet, vlan):
         network = self._execute(self.idl.ls_add(switch=name, may_exist=False))
         localnet_port = self._create_port(
             ovnconst.LOCALNET_SWITCH_PORT_NAME, str(network.uuid))
         self._set_port_localnet_values(localnet_port, localnet, vlan)
-        self._set_network_localnet_values(network, localnet, vlan)
-        updated_network = self._get_network(str(network.uuid))
-        return updated_network
+        updated_network = self._get_ls(str(network.uuid))
+        return self._get_network(updated_network)
 
     @NetworkMapper.validate_update
     @NetworkMapper.map_from_rest
@@ -141,8 +152,8 @@ class OvnNorth(object):
         return self.get_network(network_id)
 
     def _update_localnet_on_network(self, network_id, localnet, vlan):
-        network = self._get_network(network_id)
-        localnet_port = self._get_localnet_port(network)
+        network = self._get_ls(network_id)
+        localnet_port = self._get_localnet_lsp(network)
         if localnet:
             if not localnet_port:
                 localnet_port = self._create_port(
@@ -150,23 +161,6 @@ class OvnNorth(object):
             self._set_port_localnet_values(localnet_port, localnet, vlan)
         elif localnet_port:
             self._delete_port(str(localnet_port.uuid))
-        self._set_network_localnet_values(network, localnet, vlan)
-
-    def _set_network_localnet_values(self, network, localnet=None, vlan=None):
-        db_set_command = DbSetCommand(
-            self.idl, ovnconst.TABLE_LS, network.uuid
-        )
-        db_set_command.add(
-            ovnconst.ROW_LS_EXTERNAL_IDS,
-            {NetworkMapper.OVN_LOCALNET: localnet},
-            localnet
-        )
-        db_set_command.add(
-            ovnconst.ROW_LS_EXTERNAL_IDS,
-            {NetworkMapper.OVN_VLAN: str(vlan)},
-            vlan
-        )
-        db_set_command.execute()
 
     def _set_port_localnet_values(self, port, localnet, vlan):
         db_set_command = DbSetCommand(self.idl, ovnconst.TABLE_LSP, port.uuid)
@@ -187,7 +181,7 @@ class OvnNorth(object):
         if not network:
             raise RestDataError('Network %s does not exist' % network_id)
         if network.ports:
-            localnet_port = self._get_localnet_port(network)
+            localnet_port = self._get_localnet_lsp(network)
             only_localnet_port = len(network.ports) == 1 and localnet_port
             if not only_localnet_port:
                 raise RestDataError(
@@ -204,10 +198,10 @@ class OvnNorth(object):
 
         self._execute(self.idl.ls_del(network_id))
 
-    def _get_localnet_port(self, network):
-        for port in network.ports:
-            if port.type == ovnconst.LSP_TYPE_LOCALNET:
-                return port
+    def _get_localnet_lsp(self, ls):
+        for lsp in ls.ports:
+            if lsp.type == ovnconst.LSP_TYPE_LOCALNET:
+                return lsp
         return None
 
     @PortMapper.map_to_rest
@@ -515,7 +509,7 @@ class OvnNorth(object):
         dns=None,
     ):
         try:
-            network = self._get_network(network_id)
+            network = self._get_ls(network_id)
         except ElementNotFoundError:
             raise SubnetConfigError('Subnet can not be created, network {}'
                                     ' does not exist'.format(network_id))
@@ -613,7 +607,7 @@ class OvnNorth(object):
         network_id = subnet.external_ids.get(
             SubnetMapper.OVN_NETWORK_ID
         )
-        network = self._get_network(network_id)
+        network = self._get_ls(network_id)
         self._execute(self.idl.dhcp_options_del(subnet_id))
         for port in network.ports:
             if port.type == ovnconst.LSP_TYPE_ROUTER:
@@ -806,7 +800,7 @@ class OvnNorth(object):
         ))
 
     def _reserve_network_ip(self, network_id, gateway_ip):
-        exclude_values = self._get_network(network_id).other_config.get(
+        exclude_values = self._get_ls(network_id).other_config.get(
             ovnconst.LS_OPTION_EXCLUDE_IPS, {}
         )
         new_values = (
@@ -936,7 +930,7 @@ class OvnNorth(object):
     def _get_subnet_from_port_id(self, port_id):
         for subnet in self._list_subnets():
             network_id = subnet.external_ids[SubnetMapper.OVN_NETWORK_ID]
-            network = self._get_network(network_id)
+            network = self._get_ls(network_id)
             if any(port.name == port_id for port in network.ports):
                 return subnet
 
@@ -956,7 +950,7 @@ class OvnNorth(object):
     ):
         subnet = self._get_subnet(subnet_id)
         network_id = subnet.external_ids[SubnetMapper.OVN_NETWORK_ID]
-        network = self._get_network(network_id)
+        network = self._get_ls(network_id)
         lsp = self._get_switch_port(port_id)
         if lsp not in network.ports:
             raise ConflictError(
@@ -969,7 +963,7 @@ class OvnNorth(object):
         lr = self._get_router(router_id)
         subnet = self._get_subnet(subnet_id)
         network_id = subnet.external_ids[SubnetMapper.OVN_NETWORK_ID]
-        network = self._get_network(network_id)
+        network = self._get_ls(network_id)
         for lrp in lr.ports:
             lsp_id = self._lsp_id_by_lrp(lrp)
             lsp = self._get_switch_port(lsp_id)

provider/ovndb/ovn_north_mappers.py
@@ -24,12 +24,14 @@ from netaddr import IPNetwork
 import six
 
 from ovirt_provider_config_common import tenant_id
+import ovndb.constants as ovnconst
 import ovndb.ip as ip_utils
 from handlers.base_handler import MethodNotAllowedError
 from handlers.base_handler import BadRequestError
 
 
 NetworkPort = namedtuple('NetworkPort', ['lsp', 'ls', 'dhcp_options', 'lrp'])
+Network = namedtuple('Network', ['ls', 'localnet_lsp'])
 
 
 @six.add_metaclass(abc.ABCMeta)
@@ -101,8 +103,6 @@ class NetworkMapper(Mapper):
     NETWORK_TYPE_FLAT = 'flat'
     NETWORK_TYPE_VLAN = 'vlan'
 
-    OVN_LOCALNET = 'ovirt_ovn_localnet'
-    OVN_VLAN = 'ovirt_ovn_vlan'
     OVN_SUBNET = 'subnet'
 
     NETWORK_STATUS_ACTIVE = 'ACTIVE'
@@ -130,32 +130,39 @@ class NetworkMapper(Mapper):
         )
 
     @staticmethod
-    def row2rest(network_row):
-        if not network_row:
+    def row2rest(network):
+        if not network:
             return {}
+        ls, localnet_lsp = network.ls, network.localnet_lsp
         result = {
-            NetworkMapper.REST_NETWORK_ID: str(network_row.uuid),
-            NetworkMapper.REST_NETWORK_NAME: network_row.name,
+            NetworkMapper.REST_NETWORK_ID: str(ls.uuid),
+            NetworkMapper.REST_NETWORK_NAME: ls.name,
             NetworkMapper.REST_TENANT_ID: tenant_id(),
             NetworkMapper.REST_STATUS: NetworkMapper.NETWORK_STATUS_ACTIVE
         }
-        result.update(NetworkMapper._row2rest_localnet(network_row))
+        result.update(NetworkMapper._row2rest_localnet(localnet_lsp))
         return result
 
     @staticmethod
-    def _row2rest_localnet(network_row):
-        result = {}
-        ovn_localnet = network_row.external_ids.get(NetworkMapper.OVN_LOCALNET)
-        ovn_vlan = network_row.external_ids.get(NetworkMapper.OVN_VLAN)
-        if ovn_localnet and ovn_vlan:
-            result[NetworkMapper.REST_PROVIDER_PHYSICAL_NETWORK] = ovn_localnet
-            result[NetworkMapper.REST_PROVIDER_SEGMENTATION_ID] = int(ovn_vlan)
-            result[NetworkMapper.REST_PROVIDER_NETWORK_TYPE] = \
-                NetworkMapper.NETWORK_TYPE_VLAN
-        elif ovn_localnet:
-            result[NetworkMapper.REST_PROVIDER_PHYSICAL_NETWORK] = ovn_localnet
-            result[NetworkMapper.REST_PROVIDER_NETWORK_TYPE] = \
-                NetworkMapper.NETWORK_TYPE_FLAT
+    def _row2rest_localnet(localnet_lsp):
+        if not localnet_lsp:
+            return {}
+        result = {
+            NetworkMapper.REST_PROVIDER_PHYSICAL_NETWORK:
+                localnet_lsp.options.get(ovnconst.LSP_OPTION_NETWORK_NAME)
+        }
+        ovn_vlan = localnet_lsp.tag
+        if ovn_vlan:
+            result[NetworkMapper.REST_PROVIDER_SEGMENTATION_ID] = int(
+                ovn_vlan[0]
+            )
+            result[
+                NetworkMapper.REST_PROVIDER_NETWORK_TYPE
+            ] = NetworkMapper.NETWORK_TYPE_VLAN
+        else:
+            result[
+                NetworkMapper.REST_PROVIDER_NETWORK_TYPE
+            ] = NetworkMapper.NETWORK_TYPE_FLAT
         return result
 
     @staticmethod

provider/tests/ovntestlib.py
@@ -58,7 +58,8 @@ class OvnNetworkRow(OvnRow):
 
 class OvnPortRow(OvnRow):
     def __init__(self, uuid, name=None, external_ids=None, device_id=None,
-                 addresses=['unknown']):
+                 addresses=['unknown'], port_type=None, options=None,
+                 tag=None):
         self.uuid = uuid
         self.name = name
         self.external_ids = external_ids or {PortMapper.DEVICE_ID: device_id}
@@ -66,8 +67,9 @@ class OvnPortRow(OvnRow):
         self.addresses = addresses
         self.up = None
         self.enabled = None
-        self.type = None
-        self.options = {}
+        self.type = port_type
+        self.options = options if options else {}
+        self.tag = [tag] if tag else []
 
 
 class OvnSubnetRow(OvnRow):

provider/tests/test_ovn_north.py
@@ -62,8 +62,10 @@ class TestOvnNorth(object):
 
     PORT_ID01 = UUID(int=1)
     PORT_ID02 = UUID(int=2)
+    PORT_ID03 = UUID(int=3)
     PORT_NAME01 = 'port1'
     PORT_NAME02 = 'port2'
+    PORT_NAME03 = 'port3'
 
     PORT_1 = OvnPortRow(
         PORT_ID01,
@@ -84,6 +86,19 @@ class TestOvnNorth(object):
         }
     )
 
+    PORT_3 = OvnPortRow(
+        PORT_ID03,
+        addresses=MAC_ADDRESS,
+        external_ids={
+            PortMapper.OVN_NIC_NAME: PORT_NAME03,
+            PortMapper.OVN_DEVICE_ID: str(PORT_ID03),
+            PortMapper.OVN_DEVICE_OWNER: DEVICE_OWNER_OVIRT,
+        },
+        port_type=ovnconst.LSP_TYPE_LOCALNET,
+        options={ovnconst.LSP_OPTION_NETWORK_NAME: LOCALNET_NAME},
+        tag=LOCALNET_VLAN
+    )
+
     SUBNET_ID101 = UUID(int=101)
     SUBNET_ID102 = UUID(int=102)
 
@@ -99,13 +114,14 @@ class TestOvnNorth(object):
     NETWORK_11 = OvnNetworkRow(
         NETWORK_ID11, NETWORK_NAME11, ports=[PORT_1, PORT_2]
     )
+    NETWORK_12 = OvnNetworkRow(
+        NETWORK_ID12,
+        NETWORK_NAME12
+    )
     NETWORK_LOCALNET_12 = OvnNetworkRow(
         NETWORK_ID12,
         NETWORK_NAME12,
-        external_ids={
-            NetworkMapper.OVN_LOCALNET: LOCALNET_NAME,
-            NetworkMapper.OVN_VLAN: LOCALNET_VLAN
-        }
+        ports=[PORT_3]
     )
 
     ROUTER_ID20 = UUID(int=20)
@@ -141,9 +157,14 @@ class TestOvnNorth(object):
         }
 
     def _get_localnet_conf_from_net_row(self, net_row):
+        if not net_row.ports:
+            return {}
         conf = {}
-        physical_net = net_row.external_ids.get(NetworkMapper.OVN_LOCALNET)
-        segmentation_id = net_row.external_ids.get(NetworkMapper.OVN_VLAN)
+        localnet_port = net_row.ports[0]
+        physical_net = localnet_port.options.get(
+            ovnconst.LSP_OPTION_NETWORK_NAME
+        )
+        segmentation_id = localnet_port.tag[0] if localnet_port.tag else None
         if physical_net and segmentation_id:
             conf[NetworkMapper.REST_PROVIDER_NETWORK_TYPE] = \
                 NetworkMapper.NETWORK_TYPE_VLAN
@@ -262,7 +283,7 @@ class TestOvnNorth(object):
             None,
             False
         )
-        assert mock_db_set_command.call_count == 3
+        assert mock_db_set_command.call_count == 2
         assert mock_ls_get_command.call_count == 1
 
     @mock.patch(
@@ -294,8 +315,7 @@ class TestOvnNorth(object):
 
     @mock.patch('ovsdbapp.schema.ovn_northbound.commands.LsGetCommand')
     @mock.patch('ovsdbapp.backend.ovs_idl.command.DbSetCommand')
-    @mock.patch('ovsdbapp.schema.ovn_northbound.commands.LspAddCommand')
-    def test_update_localnet_network(self, mock_lsp_add_command,
+    def test_update_localnet_network(self,
                                      mock_set_command, mock_ls_get_command,
                                      mock_connection):
         mock_ls_get_command.return_value.execute.return_value = (
@@ -314,21 +334,12 @@ class TestOvnNorth(object):
         result = ovn_north.update_network(rest_data, TestOvnNorth.NETWORK_ID12)
 
         self.assert_networks_equal(result, TestOvnNorth.NETWORK_LOCALNET_12)
-        assert mock_set_command.call_count == 4
+        assert mock_set_command.call_count == 2
         assert mock_set_command.mock_calls[0] == mock.call(
             ovn_north.idl,
             ovnconst.TABLE_LS,
             TestOvnNorth.NETWORK_ID12,
             (NetworkMapper.REST_NETWORK_NAME, TestOvnNorth.NETWORK_NAME12)
-        )
-        assert mock_lsp_add_command.call_count == 1
-        assert mock_lsp_add_command.mock_calls[0] == mock.call(
-            ovn_north.idl,
-            str(TestOvnNorth.NETWORK_LOCALNET_12.uuid),
-            ovnconst.LOCALNET_SWITCH_PORT_NAME,
-            None,
-            None,
-            False
         )
         assert mock_ls_get_command.call_count == 2
 

commit cee5eddcf725c179c309065305fe603e7d454ac9

Fix regression during creating router with ext_fixed_ips
Fix regression during creating rotuer with ext_fixed_ips.
The regression is introduced by
1d6313bde943e44fe523a5887f13cfa32ce289db.
No unit test is added, because the unit test for adding a
router is pending.

Change-Id: I763bf87166a9f9b3cee5a6395d6d604a693dec5c
Signed-off-by: Dominik Holler <dholler@redhat.com>

provider/ovndb/ovn_north.py
@@ -642,7 +642,7 @@ class OvnNorth(object):
                 network_id, gateway_subnet_id
             )
             validate.ip_available_in_network(
-                self._get_network(network_id), gateway_ip)
+                self._get_ls(network_id), gateway_ip)
             self._reserve_network_ip(network_id, gateway_ip)
 
         router = self._execute(self.idl.lr_add(