db962a071e9e0541ed5b8c1ac1c087b038ac25e5
Marcin Mirecki <mmirecki@redhat.com>
2018-02-20 13:31:10 +0100

    build: build-artifacts must save src.rpm
    Change-Id: I7ce236037c929fbfe7c96d1fbdcccc5b5dc4f5f9
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -7,4 +7,6 @@ make rpm
 
 cp ovirt-provider-ovn-*.tar.gz exported-artifacts/
 cp ~/rpmbuild/RPMS/noarch/ovirt-provider-ovn-*.noarch.rpm exported-artifacts/
+cp ~/rpmbuild/SRPMS/ovirt-provider-ovn-*.src.rpm exported-artifacts/
+
 yum -y install exported-artifacts/ovirt-provider-ovn-*.noarch.rpm
