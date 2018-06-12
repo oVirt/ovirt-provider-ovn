fa41517c60921c55b7aac8a0fce1f79df0ccc383
Marcin Mirecki <mmirecki@redhat.com>
2018-06-12 17:21:36 +0200

    automation: add f28 support
    Added fc28 repos and fixes for working on Fedora.
    
    Change-Id: I7cb11ac6e470f2c3d320b1fc5061fe2c0cbb3076
    Signed-off-by: Marcin Mirecki <mmirecki@redhat.com>
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -xe
 
+DISTVER="$(rpm --eval "%dist"|cut -c2-3)"
+PACKAGER=""
+if [[ "${DISTVER}" == "el" ]]; then
+    PACKAGER=yum
+else
+    PACKAGER=dnf
+fi
+
+
 mkdir -p exported-artifacts
 
 mkdir -p "`rpm --eval %_topdir`" "`rpm --eval %_sourcedir`"
@@ -9,4 +18,4 @@ cp ovirt-provider-ovn-*.tar.gz exported-artifacts/
 cp ~/rpmbuild/RPMS/noarch/ovirt-provider-ovn-*.noarch.rpm exported-artifacts/
 cp ~/rpmbuild/SRPMS/ovirt-provider-ovn-*.src.rpm exported-artifacts/
 
-yum -y install exported-artifacts/ovirt-provider-ovn-*.noarch.rpm
+${PACKAGER} -y install exported-artifacts/ovirt-provider-ovn-*.noarch.rpm
