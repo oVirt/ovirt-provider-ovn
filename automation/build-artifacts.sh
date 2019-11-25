c5b646866eeee108db07ea2d43024132252c4981
Miguel Duarte Barroso <mdbarroso@redhat.com>
2019-11-25 13:41:08 +0100

    automation, build: skip driver install on el7
    ovirt-master will only run on el8 hosts. As a result, vdsm el7
    builds were removed from the tested / ovirt-master repos.
    
    To account for this, the el7 driver cannot be installed in an el7
    host.
    
    Change-Id: Ia30b7e6f78635f4d0a4b12a5d83cdb44d9b6d2d6
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -4,8 +4,12 @@ DISTVER="$(rpm --eval "%dist"|cut -c2-4)"
 PACKAGER=""
 if [[ "${DISTVER}" == "el7" ]]; then
     PACKAGER=yum
+    # el7 repos do not have the driver package, which is only released
+    # for el8
+    RPMS="exported-artifacts/ovirt-provider-ovn-[!driver]*.noarch.rpm"
 else
     PACKAGER=dnf
+    RPMS="exported-artifacts/ovirt-provider-ovn-*.noarch.rpm"
 fi
 
 
@@ -18,4 +22,4 @@ cp ovirt-provider-ovn-*.tar.gz exported-artifacts/
 cp ~/rpmbuild/RPMS/noarch/ovirt-provider-ovn-*.noarch.rpm exported-artifacts/
 cp ~/rpmbuild/SRPMS/ovirt-provider-ovn-*.src.rpm exported-artifacts/
 
-${PACKAGER} -y install exported-artifacts/ovirt-provider-ovn-*.noarch.rpm
+${PACKAGER} -y install $RPMS
