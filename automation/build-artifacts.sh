edf44adf644cb4f039c1184d88281d83bd061868
Ales Musil <amusil@redhat.com>
2021-09-21 08:46:05 +0000

    Revert "automation: forcefully install requirements"
    This reverts commit 666ab9a07ad9d298f12278b348ffbf170a7d8255.
    
    Reason for revert: This is no longer required
    
    Change-Id: Ie98f346558845c137a45d4af9de98f00534aae41

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -xe
 
-# mock runner is not setting up the system correctly
-# https://issues.redhat.com/browse/CPDEVOPS-242
-readarray -t pkgs < automation/build-artifacts.packages
-dnf install -y "${pkgs[@]}"
-
 mkdir -p exported-artifacts
 
 mkdir -p "`rpm --eval %_topdir`" "`rpm --eval %_sourcedir`"
